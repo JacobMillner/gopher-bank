3c5181126d108b48625449189e84c0cd563ab04e
Jacob Millner <jacob.millner@outlook.com>
2022-07-19 19:46:36 -0400

    generate account select code

--- db/query/account.sql
+++ db/query/account.sql
@@ -7,3 +7,14 @@ INSERT INTO accounts (
 	$1, $2, $3
 ) RETURNING *;
 
+
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAccounts :many
+SELECT * FROM accounts
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
